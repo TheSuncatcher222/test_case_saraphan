7286302e26a37fa38990a6c95f421ea0a0984e86
TheSuncatcher222 <TheSuncatcher222@gmail.com>
2023-12-11 21:56:36 +0300

    Add ShoppingCartViewSet and JWT

--- backend/api/v1/urls.py
+++ backend/api/v1/urls.py
@@ -1,9 +1,12 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 from rest_framework.viewsets import ModelViewSet
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView, TokenRefreshView,
+)
 
 from api.v1.views import (
-    CategoryViewSet, GoodViewSet, SubcategoryViewSet,
+    CategoryViewSet, GoodViewSet, ShoppingCartViewSet, SubcategoryViewSet,
 )
 
 router = DefaultRouter()
@@ -11,7 +14,13 @@ router = DefaultRouter()
 ROUTER_DATA: list[dict[str, ModelViewSet]] = [
     {'prefix': 'categories', 'viewset': CategoryViewSet},
     {'prefix': 'goods', 'viewset': GoodViewSet},
+    {'prefix': 'shopping-cart', 'viewset': ShoppingCartViewSet},
     {'prefix': 'subcategories', 'viewset': SubcategoryViewSet},
+]
+
+urlpatterns_token = [
+    path('create/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
 
 for route in ROUTER_DATA:
@@ -23,4 +32,5 @@ for route in ROUTER_DATA:
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('auth/token/', include(urlpatterns_token)),
 ]
