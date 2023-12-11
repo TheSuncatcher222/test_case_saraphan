63a4458a40f6f0cf784a1fcc26d72bcc963c6228
TheSuncatcher222 <TheSuncatcher222@gmail.com>
2023-12-11 21:56:15 +0300

    Add ShoppingCart serializers

--- backend/api/v1/serializers.py
+++ backend/api/v1/serializers.py
@@ -1,4 +1,6 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import (
+    CharField, IntegerField, ModelSerializer, ValidationError,
+)
 
 from goods.models import Category, Good, ShoppingCart, Subcategory
 
@@ -32,10 +34,28 @@ class SubcategoryGetSerializer(ModelSerializer):
         )
 
 
+class SubcategoryGetShortSerializer(ModelSerializer):
+    """
+    Сериализатор представления объектов Subcategory в усеченном виде.
+    Используется для отображения названия, цены и категории товара
+    в ShoppingCartGetSerializer.."""
+
+    parent_category = CharField(source='category.name')
+
+    class Meta:
+        model = Subcategory
+        fields = (
+            'id',
+            'name',
+            'slug',
+            'parent_category',
+        )
+
+
 class GoodGetSerializer(ModelSerializer):
     """Сериализатор представления объектов Good."""
 
-    subcategory = SubcategoryGetSerializer()
+    subcategory = SubcategoryGetShortSerializer()
 
     class Meta:
         model = Good
@@ -49,3 +69,140 @@ class GoodGetSerializer(ModelSerializer):
             'image_medium',
             'image_small',
         )
+
+
+class GoodGetShortSerializer(ModelSerializer):
+    """
+    Сериализатор представления объектов Good в усеченном виде.
+    Используется для отображения названия, цены и категории товара
+    в ShoppingCartGetSerializer.
+    """
+
+    subcategory = SubcategoryGetSerializer()
+
+    class Meta:
+        model = Good
+        fields = (
+            'id',
+            'name',
+            'price',
+            'subcategory',
+        )
+
+
+class ShoppingCartGetSerializer(ModelSerializer):
+    """
+    Сериализатор представления объектов ShoppingCart.
+    """
+
+    good = CharField(source='good.name')
+    price = IntegerField(source='good.price')
+
+    class Meta:
+        model = ShoppingCart
+        fields = (
+            'good',
+            'price',
+            'amount',
+        )
+
+
+class ShoppingCartShortSerializer(ModelSerializer):
+    """
+    Сериализатор представления объектов ShoppingCart в усеченном виде.
+    Используется для валидации названия и количества товара
+    в ShoppingCartPostListSerializer.
+    """
+
+    class Meta:
+        model = ShoppingCart
+        fields = (
+            'good',
+            'amount',
+        )
+
+
+class ShoppingCartPostListSerializer(ModelSerializer):
+    """Сериализатор добавления списка объектов ShoppingCart."""
+
+    goods = ShoppingCartShortSerializer(many=True)
+
+    class Meta:
+        model = ShoppingCart
+        fields = (
+            'user',
+            'goods',
+        )
+
+    def validate(self, attrs):
+        goods = attrs.get('goods')
+        if not goods:
+            raise ValidationError(
+                {
+                    "goods": [
+                        {
+                            "good": [
+                                "Обязательное поле."
+                            ],
+                            "amount": [
+                                "Обязательное поле."
+                            ]
+                        }
+                    ]
+                }
+            )
+        goods_names: list[str] = []
+        for good in goods:
+            goods_names.append(good['good'])
+        goods_set = set(goods_names)
+        if len(goods) != len(goods_set):
+            raise ValidationError(
+                {
+                    "goods": [
+                        {
+                            "good": [
+                                "Значение поля должно быть уникальным."
+                            ]
+                        }
+                    ]
+                }
+            )
+        return super().validate(attrs)
+
+    def create(self, validated_data):
+        shopping_items: list[ShoppingCart] = []
+        for item in validated_data['goods']:
+            shopping_items.append(
+                ShoppingCart(
+                    user=validated_data['user'],
+                    good=item['good'],
+                    amount=item['amount'],
+                )
+            )
+        objects: list[ShoppingCart] = (
+            ShoppingCart.objects.bulk_create(shopping_items)
+        )
+        self.context['objects'] = objects
+        return objects
+
+    def to_representation(self, instance):
+        objects: list[ShoppingCart] = self.context['objects']
+        total_goods = len(objects)
+        total_sum = sum(
+            object.good.price * object.amount for object in objects
+        )
+        goods: list[dict[str, any]] = []
+        for object in objects:
+            goods.append(
+                {
+                    'good': object.good.name,
+                    'price': object.good.price,
+                    'amount': object.amount,
+                }
+            )
+        data = {
+            'total_goods': total_goods,
+            'total_sum': total_sum,
+            'goods': goods,
+        }
+        return data
